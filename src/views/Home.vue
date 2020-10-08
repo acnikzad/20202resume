5d267726bd80a89dd0535cf02fab59f0885cdfa9
“Aria <a.c.nikzad@gmail.com>
2020-10-08 13:18:30 -0700

    finished hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,40 @@
 <template>
-<section id="Home" class="bannerSec overflow-hidden">
-        <!-- bannerSlide -->
-        <div class="bannerSlide d-block header1">
-          <!-- bannerWrap -->
-          <div class="bannerWrap overlay d-flex position-relative overflow-hidden">
-            <div class="alignCenter w-100 d-flex align-items-center">
-              <div class="container pt-25 pb-14 pt-sm-33 pb-sm-20">
-                <div class="caption position-relative text-center text-white">
-                  <h1 class="h1 text-uppercase text-white mb-4">
-                    <strong class="d-block font-weight-bold mb-n1 mb-sm-2">Grow your</strong>
-                    <strong class="d-block font-weight-bold">Digital Business</strong>
-                  </h1>
-                  <p>We make outstanding design with knowledge, passion, heart and soul</p>
-                  <a href="javascript:void(0);" class="btn btn-danger text-uppercase rounded-pill fwSemibold">learn more</a>
+  <section id="Home" class="bannerSec overflow-hidden">
+  <!-- bannerSlide -->
+    <div class="bannerSlide d-block header1">
+      <!-- bannerWrap -->
+      <div class="bannerWrap overlay d-flex position-relative overflow-hidden">
+        <div class="alignCenter w-100 d-flex align-items-center">
+          <div class="container pt-25 pb-14 pt-sm-33 pb-sm-20">
+            <div class="caption position-relative text-center text-white">
+              <h1 class="h1 text-uppercase text-white mb-4">
+                <div class="row hero-margin">
+                  <img src="../../public/images/2020headshotsidesquare.jpg" class="hero-pic">
+                  <div class="col">
+                    <h1 class="d-block font-weight-light mb-n1 mb-sm-2 hero-text">Aria Nikzad</h1>
+                    <h2 class="d-block font-weight-light hero-text">Full Stack Developer</h2>
+                    <div class="icon-small">
+                      <i class="devicon-ruby-plain"></i>
+                      <i class="devicon-rails-plain"></i>
+                      <i class="devicon-vuejs-plain"></i>
+                      <i class="devicon-javascript-plain"></i>
+                      <i class="devicon-css3-plain"></i>
+                      <i class="devicon-html5-plain"></i>
+                      <i class="devicon-bootstrap-plain"></i>
+                      <i class="devicon-github-plain"></i>
+                      <i class="ion-social-bitcoin"></i>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </h1>
+              <a href="javascript:void(0);" class="btn btn-danger text-uppercase rounded-pill fwSemibold">learn more</a>
             </div>
-            <!-- <span class="bgImg bgCover position-absolute" style="background-image: url(images/BurneyFalls.jpg)"></span> -->
-          </div>
-          <!-- bannerWrap -->
-          <div class="bannerWrap overlay d-flex position-relative overflow-hidden">
-            <div class="alignCenter w-100 d-flex align-items-center">
-              <div class="container pt-25 pb-14 pt-sm-33 pb-sm-20">
-                <div class="caption position-relative text-center text-white">
-                  <h1 class="h1 text-uppercase text-white mb-4">
-                    <strong class="d-block font-weight-bold mb-n1 mb-sm-2">Start Now</strong>
-                    <strong class="d-block font-weight-bold">With Our Template</strong>
-                  </h1>
-                  <p>We make outstanding design with knowledge, passion, heart and soul</p>
-                  <a href="javascript:void(0);" class="btn btn-danger text-uppercase rounded-pill fwSemibold">learn more</a>
-                </div>
-              </div>
-            </div>
-            <span class="bgImg bgCover position-absolute" style="background-image: url(images/img45.jpg)"></span>
-          </div>
-          <!-- bannerWrap -->
-          <div class="bannerWrap overlay d-flex position-relative overflow-hidden">
-            <div class="alignCenter w-100 d-flex align-items-center">
-              <div class="container pt-25 pb-14 pt-sm-33 pb-sm-20">
-                <div class="caption position-relative text-center text-white">
-                  <h1 class="h1 text-uppercase text-white mb-4">
-                    <strong class="d-block font-weight-bold mb-n1 mb-sm-2">Great Ways to </strong>
-                    <strong class="d-block font-weight-bold">Start Your Template</strong>
-                  </h1>
-                  <p>We make outstanding design with knowledge, passion, heart and soul</p>
-                  <a href="javascript:void(0);" class="btn btn-danger text-uppercase rounded-pill fwSemibold">learn more</a>
-                </div>
-              </div>
-            </div>
-            <span class="bgImg bgCover position-absolute" style="background-image: url(images/img46.jpg)"></span>
           </div>
         </div>
-      </section>
+        <span class="bgImg bgCover position-absolute" style="background-image: url(images/img45.jpg)"></span>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
